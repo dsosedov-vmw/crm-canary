1096ae58264030b22b343faf81d652a139f81cd6
Dmitrii Sosedov <dsosedov@pivotal.io>
2020-01-30 17:22:32 -0500

    Add simple CRM call

--- src/CRMApp.Web/Service1.svc.cs
+++ src/CRMApp.Web/Service1.svc.cs
@@ -1,10 +1,8 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Runtime.Serialization;
-using System.ServiceModel;
-using System.ServiceModel.Web;
-using System.Text;
+﻿using Microsoft.Crm.Sdk.Messages;
+using Microsoft.Xrm.Sdk;
+using Microsoft.Xrm.Sdk.Client;
+using System;
+using System.ServiceModel.Description;
 
 namespace CRMApp.Web
 {
@@ -12,9 +10,25 @@ namespace CRMApp.Web
     // NOTE: In order to launch WCF Test Client for testing this service, please select Service1.svc or Service1.svc.cs at the Solution Explorer and start debugging.
     public class Service1 : IService1
     {
+        IOrganizationService _service;
+
         public string GetData(int value)
         {
-            return string.Format("You entered: {0}", value);
+            ConnectToMSCRM(
+                Environment.GetEnvironmentVariable("CRMUsername"),
+                Environment.GetEnvironmentVariable("CRMPassword"),
+                Environment.GetEnvironmentVariable("CRMSoapOrgServiceUri"));
+
+            var userId = ((WhoAmIResponse)_service.Execute(new WhoAmIRequest())).UserId;
+
+            if (userId != Guid.Empty)
+            {
+                Console.WriteLine("Connection Established Successfully");
+            }
+
+            //return string.Format("You entered: {0}", value);
+
+            return userId.ToString();
         }
 
         public CompositeType GetDataUsingDataContract(CompositeType composite)
@@ -28,6 +42,25 @@ namespace CRMApp.Web
                 composite.StringValue += "Suffix";
             }
             return composite;
+        }
+
+        private void ConnectToMSCRM(string username, string password, string soapOrgServiceUri)
+        {
+            try
+            {
+                var credentials = new ClientCredentials();
+                credentials.UserName.UserName = username;
+                credentials.UserName.Password = password;
+                Uri serviceUri = new Uri(soapOrgServiceUri);
+                // See https://community.dynamics.com/crm/f/microsoft-dynamics-crm-forum/281686/how-resolve-an-existing-connection-was-forcibly-closed-by-the-remote-host-in-ssrs-report/807188 if it throws
+                var proxy = new OrganizationServiceProxy(serviceUri, null, credentials, null);
+                proxy.EnableProxyTypes();
+                _service = (IOrganizationService)proxy;
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine("Error while connecting to CRM " + ex.Message);
+            }
         }
     }
 }
